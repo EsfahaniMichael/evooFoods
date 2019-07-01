764a4c05886456bf666edd3801531e9884fc3c97
Esfahani <esfahanimichael@gmail.com>
2019-06-30 22:25:32 -0700

    Working on making the navigation bar stick correctly

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 
 
-
-$(window).load(function(){
-    $("#mainNavWrapper").sticky({ topSpacing: 0});
+$(window).load(function () {
+    $("#header").sticky({ topSpacing: 0 });
   });
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+/// Nav Bar functions
+
+var address = window.location.href;
+var page = address.substr(address.lastIndexOf('/') + 1);
+$('.main-nav a[href*="' + page + '"]').addClass('active');
